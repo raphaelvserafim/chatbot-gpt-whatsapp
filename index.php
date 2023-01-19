8ab23146cf6eb6ca0231f7f5fa0973e4a47e2206
Raphael Serafim <raphaelvserafim@gmail.com>
2023-01-18 22:21:10 -0400

    WhatsApp

--- index.php
+++ index.php
@@ -2,7 +2,30 @@
 
 use Cachesistemas\ChatbotGptWhatsapp\OpenAI;
 
+use Cachesistemas\ClassePhpApiWame\WhatsApp;
 
-$openAI = new OpenAI('');
 
-$result = $openAI->generateText("");
+include 'vendor/autoload.php';
+
+
+$openAI         = new OpenAI('Your Key');
+
+$whatsapp       = new WhatsApp(["server" => "API server", "key" => "Your Key Instance"]);
+
+$result         = $openAI->generateText("quantos dias na semana é bom para ir para academia ?");
+
+
+
+$resposta = '';
+
+if (sizeof($result["choices"]) > 0) {
+
+    for ($i = 0; sizeof($result["choices"]) > $i; $i++) {
+        $resposta .= $result["choices"][$i]["text"];
+    }
+}
+
+
+
+
+echo $resposta;
